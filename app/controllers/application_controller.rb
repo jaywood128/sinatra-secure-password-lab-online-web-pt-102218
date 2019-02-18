564b0803a65de095cc44a242e4b7314711eb1351
Johnathon Wood <jaywood128@gmail.com>
2019-02-18 18:46:32 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
     binding.pry
     #your code here
 
