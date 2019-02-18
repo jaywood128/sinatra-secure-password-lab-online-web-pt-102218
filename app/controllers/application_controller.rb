2248a20bbe4214f3cdb14fc0ae6870f2c2fd3c34
Johnathon Wood <jaywood128@gmail.com>
2019-02-18 19:45:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    
+
     if user && user.authenticate(params[:password]) && user[:username] != ""
       session[:user_id] = user.id
 			redirect "/account"
